09b0d0b81811b4e190b7665df79e79910244a2a5
Sunny <sunnysharma.mca@gmail.com>
2020-06-07 16:12:50 -0400

    test

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,6 +1,7 @@
 public class HelloJenkins {
  public static void main (String args[]){
-	 System.out.println("******Hello to Jenkins********");
+	 System.out.println("******Triggering from webhook -> Hello to Jenkins********");
+	 
  }
 
 }
